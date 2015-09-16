d9cad437e7b49c2483b773154df41acf5c5e6834
Trajche Shoposki <tsoposki@gmail.com>
2015-09-16 20:14:04 +0200

    refactor init gulp task and update README

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
     <br />
 <b>To serve project please run:</b> <br />
 gulp serve
+
+<br />
+<br />
+<b>(Temporary solution)</b>
+<br />
+Add your newly created .ts files in tsconfig.json "files" array (will be fixed soon)
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,12 @@
+// TODO: [tsoposki] will be implemented soon
 const build = require('./gulp/build');
+
 const config = require('./gulp/config');
 const serve = require('./gulp/serve');
 const styles = require('./gulp/styles');
+const init = require('.gulp/init');
 
 var gulp = require('gulp');
-var shell = require('gulp-shell');
-var runSequence = require('run-sequence');
 
-gulp.task('libs', function () {
-    var size = require('gulp-size');
-    return gulp.src(config.lib)
-        .pipe(size({showFiles: true, gzip: true}))
-        .pipe(gulp.dest('app/lib'));
-});
-
-gulp.task('install', shell.task([
-    'npm install -g typescript tsd',
-    'tsd install angular2 es6-promise rx rx-lite'
-]));
-
-gulp.task('init', function () {
-    runSequence(
-        'install',
-        'libs'
-    );
-});
 
 gulp.task('default', ['serve']);
